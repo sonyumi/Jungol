9ce372bc49595053e04ac594eb4b0dff39fe4a33
Class01 <Class01@DESKTOP-2FRFO7T>
2023-04-26 14:46:19 +0900

    503번 문제

--- Jongol/src/q503/Main.java
+++ Jongol/src/q503/Main.java
@@ -3,8 +3,7 @@ package q503;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("My name is Hong Gil Dong.");
-		System.out.println("I am 13 years old.");
+		System.out.println("My name is Hong Gil Dong.\nI am 13 years old.");
 
 	}
 
