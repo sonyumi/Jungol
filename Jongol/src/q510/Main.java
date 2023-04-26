f44f4677426e65da96eda6cfb5f1d0efe5a1cd24
Class01 <Class01@DESKTOP-2FRFO7T>
2023-04-26 17:15:56 +0900

    문제풀이

---
+++ Jongol/src/q510/Main.java
@@ -0,0 +1,13 @@
+package q510;
+
+public class Main {
+	public static void main(String[] args) {
+		int a;
+		a = -1;
+		int b;
+		b = 100;
+
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
